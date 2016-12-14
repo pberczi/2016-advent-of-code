1f305acd31018369f81374aa6a19504aa700c886
Laszlo-Peter <p.berczi@gmail.com>
2016-12-14 02:09:36 -0500

    cleanup

--- 2016/14/14.py
+++ 2016/14/14.py
@@ -15,43 +15,40 @@ data = []
 with open(args.input, 'r') as f:
   data = f.read().splitlines()
 
-def hasTriple(s):
+def getFirstTriple(s):
   for i in range(len(s)-2):
     if s[i] == s[i+1] and s[i] == s[i+2]:
-      return (True,s[i])
-  return (False, '')
+      return s[i]
+  return ''
 
 salt = 'cuanljph'
 # salt = 'abc'
 
-candidates = set()
-keys = []
+candidates = []
 key_idx = []
 i = 0
-while len(keys) < 64:
+while len(key_idx) < 64:
   hsh = md5.new(salt + str(i)).hexdigest()
   if args.problem == 2:
     for j in range(2016):
       hsh = md5.new(hsh).hexdigest()
   
-  (has_triple, letter) = hasTriple(hsh)
-  if has_triple:
+  letter = getFirstTriple(hsh)
+  if letter != '':
     # print i, letter, hsh
-    candidates.add((hsh,letter,i))
+    candidates.append((letter,i))
   
-  remaining_candidates = set()
+  remaining_candidates = []
   for candidate in candidates:
-    letter = candidate[1]
-    j = candidate[2]
+    letter = candidate[0]
+    j = candidate[1]
     if i - j <= 1000:
       if 5*letter in hsh and i - j > 0:
         # print 'found', i, letter, hsh, j
-        keys.append(candidate[0])
         key_idx.append(j)
       else:
-        remaining_candidates.add(candidate)
+        remaining_candidates.append(candidate)
   candidates = remaining_candidates
-  
   i += 1
 
 key_idx.sort()
